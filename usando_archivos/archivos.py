628b908fa8887b6ead226f70897a7d39902a6f7f
Emmanuel Alfaro <ealfarobrenes@hotmail.com>
2017-10-10 10:17:02 -0600

    insertando en archivo de terminal y mostrando lo que hay

--- usando_archivos/archivos.py
+++ usando_archivos/archivos.py
@@ -1,17 +1,30 @@
 def main():
-    f = open("file", "r")
-    i = 1
-    while(1):
+    guardar_nombres()
+
+    #f = open("file", "r")
+    #i = 1
+    #while(1):
         #s = f.readline()
-        s = f.read(8)
-        if(s!=""):
-            print("Línea"+str(i)+" = "+s)
-            i+=1
-        else:
-            break
+        #s = f.read(8)
+        #if(s!=""):
+            #print("Línea"+str(i)+" = "+s)
+            #i+=1
+        #else:
+            #break
         
     #for x in range(1,4):
         #f.write("Hola mundo"+str(x)+"\n")
-
+def guardar_nombres():
+    f = open("file","r+")
+    x = input(">")
+    while(x!="."):
+        f.write(x+"\n")
+        a = f.tell()
+        f.seek(0,0)
+        print("Archivo:")
+        print(f.read())
+        f.seek(a,0)
+        x = input(">")
+        
 
 main()
